edfcc6bc23ce76b5dcecd62b7445b7fc9c7c625b
mssliwinska <equinoxe@onet.eu>
2018-07-19 02:12:32 +0200

    update of task status enabled

--- app.py
+++ app.py
@@ -14,18 +14,35 @@ class Todo(db.Model):
     completed = db.Column(db.Boolean)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    incomplete = Todo.query.filter_by(completed=False).all()
+    complete = Todo.query.filter_by(completed=True).all()
+    return render_template('index.html', incomplete=incomplete, complete=complete)
 
 
-@app.route("/add", methods=["POST"])
+@app.route('/add', methods=['POST'])
 def add():
-    todo = Todo(text=request.form["item-to-do"], completed=False)
+    todo = Todo(text=request.form['item-to-do'], completed=False)
     db.session.add(todo)
     db.session.commit()
 
-    return redirect(url_for("index"))
+    return redirect(url_for('index'))
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<id>')
+def complete(id):
+
+    todo = Todo.query.filter_by(id=int(id)).first()
+    todo.completed = True
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
