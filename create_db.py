1984d2b0c0e2a6dd99bbe929ef2e4806a6de4d48
mssliwinska <equinoxe@onet.eu>
2018-07-22 02:46:40 +0200

    create-db module created

---
+++ create_db.py
@@ -0,0 +1,7 @@
+from app import app, db
+
+with app.app_context():
+    db.create_all()
+    print('Database tables:')
+    for table in db.metadata.sorted_tables:
+        print('- {}'.format(table))
