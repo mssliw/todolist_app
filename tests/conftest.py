a811c7656979842885db1e4da46025845528a26c
mssliwinska <equinoxe@onet.eu>
2018-07-23 12:47:01 +0200

    fixture adjustment to db model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
 import pytest
+import random
 
+from mimesis import Generic
+from random import randint
 from sqlalchemy import event
 
 from app import create_app
 from app import db as _db
+
+from models import TaskList, TaskCard, TaskDetails
+
+
+g = Generic('en')
 
 
 @pytest.fixture(scope="module")
@@ -64,4 +72,45 @@ def session(db):
     sess.remove()
     txn.rollback()
     conn.close()
+
+
+@pytest.fixture(scope="function")
+def db_task_list(session, client):
+    """
+    Creates and returns function-scoped database entry for task list
+    """
+    task_list = TaskList(
+        id=randint(1, 30),
+        list_title=' '.join(g.text.title().split(' ')[:5])
+    )
+
+    yield (task_list)
+
+
+@pytest.fixture(scope="function")
+def db_task_card(session, client, db_task_list):
+    """
+    Creates and returns function-scoped database entry for task card
+    """
+    task_card = TaskCard(
+        id=randint(1, 30),
+        card_title=' '.join(g.text.title().split(' ')[:5]),
+        task_list_id=db_task_list.id,
+    )
+
+    yield (task_card)
+
+
+@pytest.fixture(scope="function")
+def db_task_details(session, client, db_task_card):
+    """
+    Creates and returns function-scoped database entry for task details
+    """
+    task_details = TaskDetails(
+        id=randint(1, 30),
+        task_description=g.text.sentence(),
+        task_card_id=db_task_card.id,
+    )
+
+    yield (task_details)
 
